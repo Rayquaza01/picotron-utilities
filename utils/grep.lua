6dca10a8133aef096bdf0ceb28967dfa04dcef4b
Arnaught <rayquaza01@outlook.com>
2024-03-21 20:38:33 -0400

    grep: Remove literal search code, add comment on how to add it back

--- utils/grep.lua
+++ utils/grep.lua
@@ -30,6 +30,9 @@ function search_file(pattern, file, print_fname)
 		-- print("searching " .. file)
 
 		for i = 1, #lines, 1 do
+			-- if you want to make grep use exact matching instead of pattern matching
+			-- add the plain=true arg to find
+			-- https://www.lua.org/manual/5.4/manual.html#pdf-string.find
 			if lines[i]:find(pattern) then
 				if print_fname then
 					add(output, string.format("%s:%d\t\t%s", file, i, lines[i]))
@@ -37,23 +40,6 @@ function search_file(pattern, file, print_fname)
 					add(output, string.format("%d\t\t%s", i, lines[i]))
 				end
 			end
-
-
-			-- exact match, not needed anymore
-			-- for j = 1, #lines[i] - #pattern + 1, 1 do
-			-- 	if (pattern == sub(lines[i], j, j + #pattern - 1)) then
-			-- 		-- print file name if recursive
-			-- 		-- otherwise, just line number
-			-- 		if print_fname then
-			-- 			output = output .. string.format("%s:%d		%s", file, i, lines[i]) .. "\n"
-			-- 		else
-			-- 			output = output .. string.format("%d		%s", i, lines[i]) .. "\n"
-			-- 		end
-
-			-- 		-- if line matched once, stop checking
-			-- 		break
-			-- 	end
-			-- end
 		end
 	elseif (filetype == "folder") then
 		-- if folder, search recursively
