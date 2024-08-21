6b02316cb82ef8c7191a75ffb5468d8b403cc319
Arnaught <rayquaza01@outlook.com>
2024-08-21 13:23:06 -0400

    Add install and uninstall scripts using stow

---
+++ install.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+stow -d src/ --adopt -t ~/.lexaloffle/Picotron/drive/ -Sv exports --ignore="\.info\.pod"
---
+++ uninstall.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+stow -d src/ -t ~/.lexaloffle/Picotron/drive/ -Dv exports
