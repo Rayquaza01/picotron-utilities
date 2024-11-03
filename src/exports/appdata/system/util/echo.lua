5e93a3cf450450daac6f4fbce7bf88c956482bec
Arnaught <rayquaza01@outlook.com>
2024-11-03 09:48:34 -0500

    [echo] add echo command

---
+++ src/exports/appdata/system/util/echo.lua
@@ -0,0 +1,5 @@
+--[[pod_format="raw",created="2024-11-03 14:48:29",modified="2024-11-03 14:48:29",revision=0]]
+-- echo is part of Picotron Utilities
+-- https://github.com/Rayquaza01/picotron-utilities
+argv = env().argv or {}
+print(table.concat(argv, " "))
