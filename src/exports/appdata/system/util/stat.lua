0ba7e3c34b3339d93be3fb6b0835e1bb7a63592a
Arnaught <rayquaza01@outlook.com>
2024-11-03 11:43:10 -0500

    [stat] remove byte prefix, don't use precision for bytes

--- src/exports/appdata/system/util/stat.lua
+++ src/exports/appdata/system/util/stat.lua
@@ -47,13 +47,13 @@ local metadata = {}
 
 local conversions = {
 	si = {
-		prefixes = {"", "KB", "MB", "GB"},
+		prefixes = {"KB", "MB", "GB"},
 		K = 1000,
 		M = 1000000, -- 1000^2
 		G = 1000000000 -- 1000^3
 	},
 	bi = {
-		prefixes = {"", "KiB", "MiB", "GiB"},
+		prefixes = {"KiB", "MiB", "GiB"},
 		K = 1024,
 		M = 1048576, -- 1024^2
 		G = 1073741824 -- 1024^3
@@ -71,14 +71,14 @@ function sizeToReadable(sz)
 	local ctable = prefix == "bi" and conversions.bi or conversions.si
 
 	if sz > ctable.G then
-		return string.format("%.2f%s", sz / ctable.G, ctable.prefixes[4])
+		return string.format("%.2f%s", sz / ctable.G, ctable.prefixes[3])
 	elseif sz > ctable.M then
-		return string.format("%.2f%s", sz / ctable.M, ctable.prefixes[3])
+		return string.format("%.2f%s", sz / ctable.M, ctable.prefixes[2])
 	elseif sz > ctable.K then
-		return string.format("%.2f%s", sz / ctable.K, ctable.prefixes[2])
+		return string.format("%.2f%s", sz / ctable.K, ctable.prefixes[1])
 	end
 
-	return string.format("%.2f%s", sz, ctable.prefixes[1])
+	return tostr(sz)
 end
 
 for arg in all(argv) do
