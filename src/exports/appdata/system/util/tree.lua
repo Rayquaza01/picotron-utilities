50a8d2af55e0e573c05b3430cb9ccb01bce2a8bb
Arnaught <rayquaza01@outlook.com>
2025-07-21 08:05:34 -0400

    [tree] add file type icons

--- src/exports/appdata/system/util/tree.lua
+++ src/exports/appdata/system/util/tree.lua
@@ -1,8 +1,43 @@
---[[pod_format="raw",created="2024-03-22 02:07:30",modified="2024-03-22 02:08:17",revision=8]]
+--[[pod_format="raw",created="2024-03-22 02:07:30",modified="2025-02-23 03:19:47",revision=19]]
 -- tree is part of Picotron Utilities
 -- https://github.com/Rayquaza01/picotron-utilities
 
 cd(env().path)
+
+-- set to false to disable file type icons
+local ICONS = true
+
+local icons = {
+	LUA="\^:803c4e4e7e7e3c00",
+	TXT="\^:3e5276425e427e00",
+	IMAGE="\^:7e6252464a527e00",
+	BINARY="\^:00466d4d4b4be600",
+	FONT="\^:000205255755a500",
+	AUDIO="\^:003c242424363600",
+	PICOTRON="\^:3e7f5f70077d7f3e",
+	FILE="\^:3e52724242427e00",
+	FOLDER="\^:00307e7e7e7e7e00"
+}
+
+local function get_icon(ext)
+	if ext == "lua" then
+		return icons.LUA
+	elseif ext == txt or ext == md then
+		return icons.TXT
+	elseif ext == "gfx" or ext == "png" or ext == "map" or ext == "qoi" then
+		return icons.IMAGE
+	elseif ext == "pod" then
+		return icons.BINARY
+	elseif ext == "sfx" then
+		return icons.AUDIO
+	elseif ext == "p64" or ext == "p64.png" or ext == "rom" then
+		return icons.PICOTRON
+	elseif ext == "font" then
+		return icons.FONT
+	end
+
+	return icons.FILE
+end
 
 if (env().argv[1] == "--help") then
 	print("Usage: tree [dir]")
@@ -39,16 +74,23 @@ function print_folder(path, depth)
 			local col = "6" -- default color grey
 
 			local ftype, _, origin = fstat(path .. "/" .. file)
+			local ico = ""
 
 			if (ftype == "folder") then
+				ico = icons.FOLDER
 				if not file:find("%.p64$") and not file:find("%.p64%.png$") and not file:find("%.p64%.rom$") then
 					col = "e" -- pink, if folder (not p64)
 					if origin then
 						col = "b" -- green, if virtual folder like /system and /ram
 					end
+				else
+					ico = icons.PICOTRON
 				end
 
-				local filename = string.format("\f%s%s/\f7", col, file)
+				if not ICONS then
+					ico = ""
+				end
+				local filename = string.format("\f%s%s%s/\f7", col, ico, file)
 
 				add(output, indent(filename, depth))
 				print_folder(path .. "/" .. file, depth + 1)
@@ -61,7 +103,11 @@ function print_folder(path, depth)
 					col = "9" -- orange
 				end
 
-				local filename = string.format("\f%s%s\f7", col, file)
+				if ICONS then
+					ico = get_icon(file:ext())
+				end
+
+				local filename = string.format("\f%s%s%s\f7", col, ico, file)
 
 				add(output, indent(filename, depth))
 			end
